3e279e8b00c1c53a14436b1e05919e5b21f5847a
Michele Scarlato <michele.scarlato@gmail.com>
2021-12-28 10:55:00 +0900

    removing a comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 
 cd /github/workspace/src/DLF
 python3 DLFServer.py &
-# this collection has been updated, it contains 1 GET and 1 test
 newman run https://www.getpostman.com/collections/db8c865aca6d4866cdf1
